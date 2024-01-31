9f0bf6dfe0c64b95feb76ba2ce6e7bc1f9e6582a
fakhri01 <habibzade.fakhri@gmail.com>
2024-01-31 15:51:00 +0400

    triangle

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php 
+    function triangle ($count){
+            for($i = 1; $i <= $count;$i++){
+        for($j = 0; $j < $i; $j++){
+            echo "0";
+        }
+        echo "<br>";
+    }
+    }
+
+    triangle(15)
+?>
