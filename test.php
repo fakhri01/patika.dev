0bfcbfa685a0eceba6349fe258e6afb9c2834f94
fakhri01 <habibzade.fakhri@gmail.com>
2024-01-31 16:00:23 +0400

    formatting

--- test.php
+++ test.php
@@ -1,11 +1,11 @@
 <?php 
     function triangle ($count){
-            for($i = 1; $i <= $count;$i++){
-        for($j = 0; $j < $i; $j++){
-            echo "0";
+        for($i = 1; $i <= $count;$i++){
+            for($j = 0; $j < $i; $j++){
+                echo "0";
+            }
+            echo "<br>";
         }
-        echo "<br>";
-    }
     }
 
     triangle(15)
